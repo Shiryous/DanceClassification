d10696ab57e3bff2f4fbe78a3b4214960e4daec8
Antonios Monogyios <shiryous@hotmail.com>
2026-01-13 19:11:25 +0200

    Split logic of global variable names to different file

---
+++ globals.py
@@ -0,0 +1,7 @@
+## Folder Structure
+DATASET_FOLDER = 'data/BallroomData'
+SPECTROGRAM_FOLDER_PATH = f'{DATASET_FOLDER}/spectrograms'
+SPLIT_SPECTROGRAM_FOLDER_PATH = f'{DATASET_FOLDER}/split_spectrograms'
+ALL_BALLROOM_SPECTROGRAMS = f'{DATASET_FOLDER}/allBallroomSpectrograms'
+
+
